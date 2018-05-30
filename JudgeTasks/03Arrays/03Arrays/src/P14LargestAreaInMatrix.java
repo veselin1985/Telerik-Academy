0e82908395cdb321225f2d2e6db54b3af12a0998
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-05-30 12:31:14 +0300

    Judge - 03Arrays - P14LargestAreaInMatrix solution.

--- JudgeTasks/03Arrays/03Arrays/src/P14LargestAreaInMatrix.java
+++ JudgeTasks/03Arrays/03Arrays/src/P14LargestAreaInMatrix.java
@@ -1,19 +1,183 @@
-import javafx.scene.Node;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class P14LargestAreaInMatrix {
 
   public static void main(String[] args) {
 
+    InputReader console = new InputReader();
 
+    int n = console.readInt();
+    int m = console.readInt();
+
+    int[][] matrix = new int[n][m];
+    boolean[][] visited = new boolean[n][m];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        matrix[i][j] = console.readInt();
+      }
+    }
+
+    int maxNums = 0;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+
+        if(!visited[i][j])
+        maxNums = Math.max(maxNums, getArea(i, j, matrix, matrix[i][j], visited));
+      }
+    }
+    System.out.println(maxNums);
   }
 
-  public static void dfs(Node node, Node goal) {
-    if (node.equals(goal)) {
-      System.out.println(node);
+
+  private static int getArea(int row, int col, int[][] matrix, int currentValue, boolean[][] visited) {
+    if (row < 0 || col < 0 || row > matrix.length - 1
+        || col > matrix[0].length - 1 || matrix[row][col] != currentValue) {
+      return 0;
+    }
+    if(visited[row][col] ){
+      return 0;
+    }
+
+    visited[row][col] = true;
+    return 1 + getArea(row + 1, col, matrix, currentValue, visited) +
+        getArea(row - 1, col, matrix, currentValue, visited) +
+        getArea(row, col + 1, matrix, currentValue, visited) +
+        getArea(row, col - 1, matrix, currentValue, visited);
 
 
+  }
+  static class InputReader {
+
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+
+    InputReader() {
+      this.stream = System.in;
+    }
+
+    int read() {
+      if (numChars == -1)
+        throw new InputMismatchException();
+      if (curChar >= numChars) {
+        curChar = 0;
+        try {
+          numChars = stream.read(buf);
+        } catch (IOException e) {
+          throw new InputMismatchException();
+        }
+        if (numChars <= 0)
+          return -1;
+      }
+      return buf[curChar++];
+    }
+
+    int readInt() {
+      int c = read();
+      while (isSpaceChar(c)) {
+        c = read();
+      }
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = read();
+      }
+      int res = 0;
+      do {
+        if (c < '0' || c > '9') {
+          throw new InputMismatchException();
+        }
+        res *= 10;
+        res += c - '0';
+        c = read();
+      } while (!isSpaceChar(c));
+      return res * sgn;
+    }
+
+    long readLong() {
+      int c = read();
+      while (isSpaceChar(c)) {
+        c = read();
+      }
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = read();
+      }
+      long res = 0;
+      do {
+        if (c < '0' || c > '9') {
+          throw new InputMismatchException();
+        }
+        res *= 10;
+        res += c - '0';
+        c = read();
+      } while (!isSpaceChar(c));
+      return res * sgn;
+    }
+
+    double readDouble() {
+      int c = read();
+      while (isSpaceChar(c)) {
+        c = read();
+      }
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = read();
+      }
+      double res = 0;
+      while (!isSpaceChar(c) && c != '.' && c != ',') {
+        if (c == 'e' || c == 'E') {
+          return res * Math.pow(10, readInt());
+        }
+        if (c < '0' || c > '9') {
+          throw new InputMismatchException();
+        }
+        res *= 10;
+        res += c - '0';
+        c = read();
+      }
+      if (c == '.' || c == ',') {
+        c = read();
+        double m = 1;
+        while (!isSpaceChar(c)) {
+          if (c == 'e' || c == 'E') {
+            return res * Math.pow(10, readInt());
+          }
+          if (c < '0' || c > '9') {
+            throw new InputMismatchException();
+          }
+          m /= 10;
+          res += (c - '0') * m;
+          c = read();
+        }
+      }
+      return res * sgn;
+    }
+
+    String readLine() {
+      int c = read();
+      while (isSpaceChar(c))
+        c = read();
+      StringBuilder res = new StringBuilder();
+      do {
+        res.appendCodePoint(c);
+        c = read();
+      } while (!isSpaceChar(c));
+      return res.toString();
+    }
+
+    boolean isSpaceChar(int c) {
+      return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
     }
   }
 }
-
 
