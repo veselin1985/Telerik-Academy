a25aa733e5dc2e33f6ab101944601d726e3bfe96
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-06-30 20:35:21 +0300

    Form adults and children menu updated with select option. Book form resposive update.

--- Projects/Single-page app project/js/weatherAJAX.js
+++ Projects/Single-page app project/js/weatherAJAX.js
@@ -1,26 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + "Faaa" +
             "&units=metric&APPID=e859adf4ab9ae89323535d40291e7ac1",
         type: "GET",
         dataType: "jsonp",
         success: function (data) {
-            var cityName = data.name;
-            var country = data.sys.country;
+            console.log(data);
             var weather = data.weather[0].main;
             var description = data.weather[0].description;
-            var temp = data.main.temp;
-            var maxTemp = data.main.temp_max;
-            var minTemp = data.main.temp_min;
-            $("#tempreture").html("City: " + cityName + "</br>" +
-                "Country: " + country + "</br>" +
-                "Weather: " + weather + "</br>" +
-                "Description: " + description + "</br>" +
-                "Current temp: " + temp + "</br>" +
-                "MaxTemp: " + maxTemp + "</br>" +
-                "MinTemp: " + minTemp + "</br>"
-            );
+            var icon = data.weather[0].icon;
+            var tempreture = data.main.temp;
+            var clouds = data.clouds.all;
+            var humidity = data.main.humidity;
+            var wind = data.wind.speed;
+            var pressure = data.main.pressure;
+
+            $("#weather-description").html("Weather: " + weather + " - " + description);
+            $("#weather-icon").attr("src", icon);
+            $("#weather-tempreture").html("Current tempreture: " + tempreture);
+            $("#weather-clouds").html("Clouds: " + humidity + "%");
+            $("#weather-humidity").html("Humidity: " + humidity + "%");
+            $("#weather-wind").html("Wind speed: "+ wind+"meter/sec");
+            $("#weather-pressure").html("Pressure: "+ pressure+"hPa");
+
         },
 
-    });  
+    });
 })
