601b5897f6c3a304e95e5300a3f4aab192f1d650
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-07-01 09:12:31 +0300

    Phone validation update

--- Projects/Single-page app project/js/bookValidation.js
+++ Projects/Single-page app project/js/bookValidation.js
@@ -4,42 +4,38 @@ function validateForm() {
         return false;
     }
     if (isEmpty($("#lastName").val().trim())) {
-        alert('Last name is required!');
+        alert("Last name is required!");
         return false;
     }
-    if (isEmpty(($("#phone").val().trim()))) {
-        alert('Phone is required!');
+    if (isEmpty($("#phone").val().trim())) {
+        alert("Phone is required!");
+        return false;
+    }
+    if (!validatePhone(($("#phone").val().trim()))) {
+        alert("Phone field must contains only digits, '+' or '-' ! No spaces are allowed!");
         return false;
     }
     if (isEmpty($("#email").val().trim())) {
-        alert('Email is required!');
+        alert("Email is required!");
         return false;
     }
     if (!validateEmail($("#email").val().trim())) {
-        alert('Email must be a valid email address!');
+        alert("Email must be a valid email address!");
         return false;
     }
     if (isEmpty($("#arrivalDate").val().trim())) {
-        alert('Arrival date is required!');
+        alert("Arrival date is required!");
         return false;
     }
     if (isEmpty($("#departureDate").val().trim())) {
-        alert('Departure date is required!');
-        return false;
-    }
-    if (isEmpty($("#adults").val().trim())) {
-        alert('Number of adults is required!');
-        return false;
-    }
-    if (isEmpty($("#children").val().trim())) {
-        alert('Number of children is required!');
+        alert("Departure date is required!");
         return false;
     }
     if (($("#adults").val().trim()) == "0" && ($("#children").val().trim()) == "0") {
         alert('Both Children and Adults cannot be 0!');
         return false;
     }
-    if($("#arrivalDate").val().trim() >= $("#departureDate").val().trim()){
+    if ($("#arrivalDate").val().trim() >= $("#departureDate").val().trim()) {
         alert("Arrival date cannot be after or equal to Departure date!")
         return false
     }
@@ -52,6 +48,11 @@ function isEmpty(str) {
 }
 
 function validateEmail(email) {
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
-    return isEmpty(email) || re.test(email);
+    var mail_pattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
+    return isEmpty(email) || mail_pattern.test(email);
+}
+
+function validatePhone(phone) {
+    var phone_pattern = /^[0-9-+]+$/ ;
+    return isEmpty(phone) || phone_pattern.test(phone);
 }
